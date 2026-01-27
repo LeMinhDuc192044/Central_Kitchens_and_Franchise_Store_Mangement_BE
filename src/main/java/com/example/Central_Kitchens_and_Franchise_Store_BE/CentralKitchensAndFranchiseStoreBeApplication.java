c8d02b99d553caae502ee088cc23c3d5e6ed1ead
anselamkeykamona <sit96leminhduc@gmail.com>
2026-01-27 07:41:10 +0700

    Create project

---
+++ src/main/java/com/example/Central_Kitchens_and_Franchise_Store_BE/CentralKitchensAndFranchiseStoreBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.Central_Kitchens_and_Franchise_Store_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentralKitchensAndFranchiseStoreBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CentralKitchensAndFranchiseStoreBeApplication.class, args);
+	}
+
+}
