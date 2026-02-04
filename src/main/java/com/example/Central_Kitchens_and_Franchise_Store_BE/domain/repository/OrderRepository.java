0f6891ba9292028dcdf53a1e47851e6404b6291e
Tran Duy Minh <tduyminh0112@gmail.com>
2026-02-04 10:52:29 +0700

    add order repo

---
+++ src/main/java/com/example/Central_Kitchens_and_Franchise_Store_BE/domain/repository/OrderRepository.java
@@ -0,0 +1,10 @@
+package com.example.Central_Kitchens_and_Franchise_Store_BE.domain.repository;
+
+import com.example.Central_Kitchens_and_Franchise_Store_BE.domain.entities.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface OrderRepository extends JpaRepository<Order, String> {
+    List<Order> findByStoreId(String storeId);
+}
