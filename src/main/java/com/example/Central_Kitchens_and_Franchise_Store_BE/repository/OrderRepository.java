72515263a80e34bd8004a7663a123d14791afab2
Tran Duy Minh <tduyminh0112@gmail.com>
2026-02-11 11:35:52 +0700

    Revert "Add confirm Order, get All Order with pending status"
    This reverts commit c112bc96f8a96977acd9d04fee6b7af023847828.

--- src/main/java/com/example/Central_Kitchens_and_Franchise_Store_BE/repository/OrderRepository.java
+++ src/main/java/com/example/Central_Kitchens_and_Franchise_Store_BE/repository/OrderRepository.java
@@ -1,7 +1,6 @@
 package com.example.Central_Kitchens_and_Franchise_Store_BE.repository;
 
 import com.example.Central_Kitchens_and_Franchise_Store_BE.domain.entities.Order;
-import com.example.Central_Kitchens_and_Franchise_Store_BE.domain.enums.OrderStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -9,6 +8,4 @@ import java.util.List;
 public interface OrderRepository extends JpaRepository<Order, String> {
     List<Order> findByStoreId(String storeId);
     Order findTopByOrderByOrderIdDesc();
-    List<Order> findByStoreIdAndStatusOrder(String storeId, OrderStatus statusOrder);
-    List<Order> findByStatusOrder(OrderStatus statusOrder);
 }
