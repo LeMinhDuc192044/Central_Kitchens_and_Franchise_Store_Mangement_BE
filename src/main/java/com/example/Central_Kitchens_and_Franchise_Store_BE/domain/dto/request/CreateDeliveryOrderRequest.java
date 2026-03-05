48102c3aed87479858c56df1f0afabdb99e9bc76
Tran Duy Minh <tduyminh0112@gmail.com>
2026-03-05 14:48:11 +0700

    edit

--- src/main/java/com/example/Central_Kitchens_and_Franchise_Store_BE/domain/dto/request/CreateDeliveryOrderRequest.java
+++ src/main/java/com/example/Central_Kitchens_and_Franchise_Store_BE/domain/dto/request/CreateDeliveryOrderRequest.java
@@ -4,14 +4,18 @@ import com.example.Central_Kitchens_and_Franchise_Store_BE.domain.enums.Required
 import com.example.Central_Kitchens_and_Franchise_Store_BE.integration.ghn.GhnItem;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 import java.util.Map;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class CreateDeliveryOrderRequest {
 
